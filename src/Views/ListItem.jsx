b209a62b2f8b665de4153345f581dde52f5168a4
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-07-03 14:41:19 +0200

    Fixed ListItem Bullet point with svg

--- src/Views/ListItem.jsx
+++ src/Views/ListItem.jsx
@@ -1,12 +1,19 @@
 import "../global-style.css";
-//import "./Heading1.css";
+import "./ListItem.css";
+
 export default function ListItem(props) {
     return (
         <div className="list_item container m-top-s m-bottom-s">
             <div className="text_column">
-                <li>
-                    {props.text}
-                </li>
+                <div className="bullet_point_row">
+                    <div className="bullet_point">
+                        <svg width="8" height="8" viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="4" cy="4" r="4" fill="black" />
+                        </svg>
+                    </div>
+                    <p>{props.text}</p>
+                </div>
+
             </div>
         </div>
     )
