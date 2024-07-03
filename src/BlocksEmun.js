6645199107755236812e2dba787462b6f800cf99
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-07-03 13:50:29 +0200

    Implemented Block layout with Enum

---
+++ src/BlocksEmun.js
@@ -0,0 +1,10 @@
+const BLOCK_LAYOUT = Object.freeze({
+    HEADING_1 : "heading_1",
+    HEADING_2 : "heading_2",
+    HEADING_3 : "heading_3",
+    PARAGRAPH : "paragraph",
+    LIST_ITEM : "list_item",
+    CALLOUT : "callout",
+    EMBED : 'embed'
+})
+export default BLOCK_LAYOUT;
--- src/Presenters/LayOutSandBox.jsx
+++ src/Presenters/LayOutSandBox.jsx
@@ -6,21 +6,45 @@ import Paragraph from "../Views/Paragraph";
 import ListItem from "../Views/ListItem";
 import "../global-style.css";
 import "./LayOutSandBox.css";
+import BLOCK_LAYOUT from "../BlocksEmun";
 
 function LayOutSandBox(props) {
 
     props.model.projectsCollection = data;
+
+    function blockRendererCB(block) {
+        switch (block.type) {
+            case BLOCK_LAYOUT.HEADING_1:
+                return <Heading1 text={block.plain_text} />
+            case BLOCK_LAYOUT.HEADING_2:
+                return <Heading2 text={block.plain_text} />
+            case BLOCK_LAYOUT.HEADING_3:
+                return <Heading3 text={block.plain_text} />
+            case BLOCK_LAYOUT.PARAGRAPH:
+                return <Paragraph text={block.plain_text} />
+            case BLOCK_LAYOUT.LIST_ITEM:
+                return (<span>List Items Pending ⚠️</span>)
+            case BLOCK_LAYOUT.EMBED:
+                return (<span>Embed Pending ⚠️</span>)
+            case BLOCK_LAYOUT.CALLOUT:
+                return (<span>Callout Pending ⚠️</span>)
+        }
+
+    }
+
     return (
         <div className="layout_sand_box container">
-            <h1>Sandbox</h1>
-            <Heading1 text={props.model.projectsCollection.projectName} />
-            <Heading2 text={props.model.projectsCollection.clients[0]} />
-            <Heading3 text={props.model.projectsCollection.date} />
-            <Paragraph text={props.model.projectsCollection.shortDescription} />
-            <ListItem text={props.model.projectsCollection.shortDescription} />
-
+            {props.model.projectsCollection.contentBlocks.map(blockRendererCB)}
         </div>
     )
 }
 
 export default LayOutSandBox;
+/*
+<h1>Sandbox</h1>
+            <Heading1 text={props.model.projectsCollection.projectName} />
+            <Heading2 text={props.model.projectsCollection.clients[0]} />
+            <Heading3 text={props.model.projectsCollection.date} />
+            <Paragraph text={props.model.projectsCollection.shortDescription} />
+            <ListItem text={props.model.projectsCollection.shortDescription} />
+*/
