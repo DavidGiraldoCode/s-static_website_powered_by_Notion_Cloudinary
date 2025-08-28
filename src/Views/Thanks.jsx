4b53d33e028dc6a4fd6e43761016349a26608dbe
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2025-08-28 07:24:30 +0200

    Fixed footer's message

--- src/Views/Thanks.jsx
+++ src/Views/Thanks.jsx
@@ -3,10 +3,12 @@ import "../global-style.css";
 export default function Thanks(props) {
     return (
         <div className="heading_3 container m-top-m m-bottom-s">
-            <div className="text_column">
-                <h4>
-                    Thanks for reading
-                </h4>
+            <div className="text_column row">
+                {/* <h1 className="callout_h1">
+                    {props.message}
+                    </h1>  */}
+                <p className="p-small">Made in Sweden by a Colombian ☕️</p>
+                <p className="p-small">© 2025 David Giraldo</p>
             </div>
         </div>
     )
